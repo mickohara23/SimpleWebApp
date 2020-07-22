e2878aae5b5afef8233278dcd4954ffa4b4a2160
Mick O'Hara <michael@ezops.com>
2020-07-22 12:58:50 +0100

    Add client & basic cross origin policy

---
+++ client-app/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axois from 'axios'
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axois.get('http://localhost:5000/api/values')
+    .then((response)=> {
+      this.setState({
+        values: response.data
+      })
+    })
+  } 
+
+  render(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <ul>
+        {this.state.values.map((value:any)=> (
+          <li> {value.name}</li>
+        ))}
+      </ul>
+      </header>
+    </div>
+  );
+  }
+}
+
+export default App;
